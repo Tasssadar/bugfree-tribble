c6af0c9e92d82857af4fd11ffc32c613a3e3b3b2
Vojtech Bocek <vbocek@gmail.com>
2013-11-07 21:38:52 +0100

    Make cp_drawables.sh work with any Android app folder

--- cp_drawable.sh
+++ cp_drawable.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 FOLDER_LIGHT="/home/tassadar/Android_Design_Icons_20120229/All_Icons/holo_light/"
 FOLDER_DARK="/home/tassadar/Android_Design_Icons_20120229/All_Icons/holo_dark/"
-LORRIS_FOLDER="/home/tassadar/lorris_mobile"
 DARK=1
 FORCE=0
 FOLDER=""
 DPI_LIST=( mdpi hdpi xhdpi )
-
 
 for arg in "$@"
 do
@@ -36,21 +34,20 @@ do
     fi
 
     new_name="$(echo $arg | cut -d'-' -f2-999 | tr '-' '_')"
-    
 
     echo "$arg to $new_name..."
 
     for dpi in ${DPI_LIST[@]}
     do
         name=$new_name
-        if [ $FORCE -eq 0 ] && [ -e $LORRIS_FOLDER/res/drawable-$dpi/$name ]; then
+        if [ $FORCE -eq 0 ] && [ -e res/drawable-$dpi/$name ]; then
             if [ $DARK -eq 1 ]; then
                 name="${name%.png}_dark.png"
             else
                 name="${name%.png}_light.png"
             fi
         fi
-        cmd="cp $FOLDER$dpi/$arg $LORRIS_FOLDER/res/drawable-$dpi/$name"
+        cmd="cp $FOLDER$dpi/$arg res/drawable-$dpi/$name"
         echo "$cmd"
         $($cmd)
     done
