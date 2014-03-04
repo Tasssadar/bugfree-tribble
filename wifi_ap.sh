b411eed43623ccafd68a91a53d7c1211c8626b9c
Vojtech Bocek <vbocek@gmail.com>
2014-03-04 18:34:34 +0100

    wifi_ap.sh: forward eth0 to the wifi network

--- wifi_ap.sh
+++ wifi_ap.sh
@@ -5,8 +5,18 @@ if [ $# -eq 0 ] || [ "$1" = "on" ]; then
     /etc/init.d/isc-dhcp-server restart
     echo "1" > /proc/sys/net/ipv4/ip_forward
     hostapd -d -B /etc/hostapd/hostapd.conf
+
+    # forward eth0
+    iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
+    iptables -I FORWARD -i wlan0 -s 10.10.0.0/24 -j ACCEPT
+    iptables -I FORWARD -i eth0 -d 10.10.0.0/24 -j ACCEPT
 else
     echo "stopping access point..."
+
+    iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+    iptables -D FORWARD -i wlan0 -s 10.10.0.0/24 -j ACCEPT
+    iptables -D FORWARD -i eth0 -d 10.10.0.0/24 -j ACCEPT
+
     /etc/init.d/isc-dhcp-server stop
     echo "0" > /proc/sys/net/ipv4/ip_forward
     killall -2 hostapd
