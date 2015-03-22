2b2c13f0207684562dde225f96b255d83f33c4a7
Vojtech Bocek <vbocek@gmail.com>
2015-03-22 16:20:04 +0100

    transporter: use different gstream params

--- transporter.sh
+++ transporter.sh
@@ -8,4 +8,5 @@ fi
 if [ $# -gt 1 ]; then
     PORT=$2
 fi
-gst-launch -v playbin2 uri=rtsp://$1:$PORT/android.dsp uridecodebin0::source::rtpbin0::latency=10
+#gst-launch -v playbin2 uri=rtsp://$1:$PORT/android.dsp uridecodebin0::source::rtpbin0::latency=10
+gst-launch rtspsrc location=rtsp://$1:$PORT/android.dsp ! rtph264depay ! ffdec_h264 ! xvimagesink sync=false
